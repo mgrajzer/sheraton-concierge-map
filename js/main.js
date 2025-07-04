ab65f7f7cb985167184013530034d69761d2f456
mgrajzer <maciej.grajzer@stud.plus.ac.at>
2025-07-04 02:20:57 +0200

    0.0.1 First Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var BasemapAT_orthofoto = L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmaporthofoto30cm/{type}/google3857/{z}/{y}/{x}.{format}', {
+	maxZoom: 20,
+	attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>',
+	type: 'normal',
+	format: 'jpeg',
+	bounds: [[46.35877, 8.782379], [49.037872, 17.189532]]
+});
+var map = L.map('map', {
+  center: [47.8069503, 13.0406775],
+  zoom: 16,
+  layers: [BasemapAT_orthofoto]
+});
